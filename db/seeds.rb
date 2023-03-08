0685148414584a23d70adacce59c6a95bb30cacd
Vinleon Ang <javadripdev@gmail.com>
2023-03-08 16:11:31 +0800

    Yelp add fake reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,20 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-5.times do
-  Restaurant.create!(
-    name: Faker::Restaurant.name,
-    address: Faker::Address.full_address,
-    phone_number: Faker::PhoneNumber.cell_phone,
-    category: %w[chinese italian japanese french belgian].sample
-  )
+# 5.times do
+#   Restaurant.create!(
+#     name: Faker::Restaurant.name,
+#     address: Faker::Address.full_address,
+#     phone_number: Faker::PhoneNumber.cell_phone,
+#     category: %w[chinese italian japanese french belgian].sample
+#   )
+# end
+
+Restaurant.all.each do |r|
+  rand(1..5).times do
+    r.reviews.create!(
+      rating: rand(1..5),
+      content: Faker::Restaurant.review
+    )
+  end
 end
