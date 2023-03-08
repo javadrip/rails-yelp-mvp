8ea0c579b286a375bf5bcdb44aff056184c018eb
Vinleon Ang <javadripdev@gmail.com>
2023-03-08 15:06:45 +0800

    Yelp generate controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  resources :restaurants, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
